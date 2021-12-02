787769d34734c1580aabd4ac9047be6a18825b3c
fawzirjili <rjilifawzi@gmail.com>
2021-12-02 22:52:25 +0100

    adding sprites and Bird class

---
+++ Bird_Class.py
@@ -0,0 +1,54 @@
+import pygame
+import random
+class Bird(pygame.sprite.Sprite):
+    birds = [["images/redbird-downflap.png","images/redbird-midflap.png","images/redbird-upflap.png"],["images/yellowbird-downflap.png","images/yellowbird-midflap.png","images/yellowbird-upflap.png"],["images/bluebird-downflap.png","images/bluebird-midflap.png","images/bluebird-upflap.png"]]
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.game_over=False
+        self.index=0
+        self.Flying = False
+        self.counter=0
+        self.images=random.choice(self.birds)
+        self.images =[pygame.image.load(i) for i in self.images]
+        self.image= self.images[self.index]
+        self.x=x
+        self.y=y
+        self.vel=0
+    def update(self):
+        if self.Flying:
+            if self.vel<=4:
+                self.vel+=0.005
+            if self.y <=370 :
+                self.y+= (self.vel)
+            if self.y<0:
+                self.y=0
+            if self.counter >= 50:
+                self.index+=1
+                self.index%=3
+                self.counter=0
+                self.image=self.images[self.index]
+            else:
+                self.counter+=1
+            
+            #rotate the bird
+            self.image =pygame.transform.rotate(self.images[self.index],self.vel*-45)
+        if self.game_over:
+            self.image =pygame.transform.rotate(self.images[self.index],-90)
+
+    def display(self,screen):
+        screen.blit(self.image,(self.x,self.y))
+    def jump(self):
+        self.vel= -0.7
+    def start_flying(self):
+        self.Flying=True
+    def is_not_flying(self):
+        return self.Flying == False
+    def is_flying(self):
+        return self.Flying
+    def check_if_game_over(self):
+        if self.y>=370:
+            self.Flying=False
+            self.game_over=True
+    def game_is_over(self):
+        return self.game_over
+
--- main.py
+++ main.py
@@ -1,16 +1,46 @@
 from typing_extensions import runtime
 import pygame
-from pygame.constants import QUIT
+from pygame.constants import QUIT, SRCCOLORKEY
+import random
+import time
+from Bird_Class import Bird
 
-weidth = 864
-height = 720
-
+weidth = 280
+height = 500
+ground_scroll = 0
+scroll_speed = 0.5
 screen=pygame.display.set_mode((weidth,height))
 pygame.display.set_caption("Flappy Bird")
-
+base = pygame.image.load("images/base.png")
+sky=pygame.image.load("images/background-day.png")
+birds =["images/redbird-downflap.png","images/yellowbird-downflap.png","images/bluebird-downflap.png"]
+icon = pygame.image.load("images/flappy.ico")
+pygame.display.set_icon(icon)
+bird_path = random.choice(birds)
+bird = pygame.image.load(bird_path)
 running = True
-
+gameover=pygame.image.load("images/gameover.png")
+bird = Bird(20,200)
+i=0
 while running:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            bird.jump()
+            if bird.is_not_flying():
+                bird.start_flying()
+    screen.blit(sky,(0,0))
+    #screen.blit(bird,(50,50))
+    if bird.is_flying():
+        ground_scroll-= scroll_speed
+    if abs(ground_scroll) > 20 :
+        ground_scroll = 0
+    bird.display(screen)
+    bird.update()
+    bird.check_if_game_over()
+    if bird.game_is_over():
+        screen.blit(gameover,(40,100))
+    screen.blit(base, (ground_scroll, 400)) 
+    pygame.display.update()
